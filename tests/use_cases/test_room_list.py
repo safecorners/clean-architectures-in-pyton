bbf768eb09f38b6e762fd9536b3e8abf4ed277cf
safecorners <safeconers.mobile@gmail.com>
2023-02-06 00:31:32 +0900

    implement Response and Request Object

--- tests/use_cases/test_room_list.py
+++ tests/use_cases/test_room_list.py
@@ -4,6 +4,7 @@ from unittest import mock
 
 from rentomatic.domain.room import Room
 from rentomatic.use_cases.room_list import room_list_use_case
+from rentomatic.requests.room_list import build_room_list_request
 
 @pytest.fixture
 def domain_rooms():
@@ -45,7 +46,10 @@ def test_room_list_without_parameters(domain_rooms):
     repo = mock.Mock()
     repo.list.return_value = domain_rooms
 
-    result = room_list_use_case(repo)
+    request = build_room_list_request()
 
+    response = room_list_use_case(repo, request)
+
+    assert bool(response) is True
     repo.list.assert_called_with()
-    assert result == domain_rooms
+    assert response.value == domain_rooms
